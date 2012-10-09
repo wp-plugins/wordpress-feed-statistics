e7674084e12b9936fb24c1195a6f18b9f0b44484
cfinke <cfinke@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-09 05:26:00 +0000

    Fixing uninstall script: proper SQL syntax, adding an old table to drop, adding opening PHP bracket.
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-feed-statistics/trunk@609879 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -1,3 +1,5 @@
+<?php
+
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
 	exit;
 
@@ -6,7 +8,10 @@ delete_option( 'feed_statistics_track_clickthroughs' );
 delete_option( 'feed_statistics_track_postviews' );
 delete_option( 'feed_statistics_expiration_days' );
 
-$wpdb->query( "DELETE TABLE ".$wpdb->prefix."feed_clickthroughs" );
-$wpdb->query( "DELETE TABLE ".$wpdb->prefix."feed_links" );
-$wpdb->query( "DELETE TABLE ".$wpdb->prefix."feed_postviews" );
-$wpdb->query( "DELETE TABLE ".$wpdb->prefix."feed_subscribers" );
+global $wpdb;
+
+$wpdb->query( "DROP TABLE ".$wpdb->prefix."feed_clickthroughs" );
+$wpdb->query( "DROP TABLE ".$wpdb->prefix."feed_links" );
+$wpdb->query( "DROP TABLE ".$wpdb->prefix."feed_postviews" );
+$wpdb->query( "DROP TABLE ".$wpdb->prefix."feed_subscribers" );
+$wpdb->query( "DROP TABLE ".$wpdb->prefix."feed_referrers" );
